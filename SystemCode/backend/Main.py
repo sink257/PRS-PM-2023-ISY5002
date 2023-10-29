655a048ea00666c6e233e9f8082de6b3fee41b9b
root <root@ip-172-31-23-18.ap-southeast-2.compute.internal>
2023-10-29 15:12:07 +0000

    Added ResNet model and majority voting result

--- SystemCode/backend/Main.py
+++ SystemCode/backend/Main.py
@@ -28,18 +28,18 @@ def load_image_cnn(bytes_data):
     np_img = np.array(img) / 255.0
     return np_img
 
-def svm_classifier(np_img):
+def svm_classifier(np_img, loaded_classifier):
     np_img = np_img.reshape(-1)
-    with open('svm_classifier_updated_gridsearch.pkl', 'rb') as f:
-        loaded_classifier = pickle.load(f)
+    # with open('svm_classifier_updated_gridsearch.pkl', 'rb') as f:
+    #     loaded_classifier = pickle.load(f)
     result = int(loaded_classifier.predict([np_img])[0])
     proba = float(loaded_classifier.predict_proba([np_img])[0][result])
     print('svm', result, proba)
     return proba, result
 
-def cnn_classifier(np_img):
+def cnn_classifier(np_img, loaded_classifier):
     np_img = np.expand_dims(np_img, axis=0)
-    loaded_classifier = tf.keras.models.load_model('my_model_200.keras')
+    # loaded_classifier = tf.keras.models.load_model('my_model_200.keras')
     proba = float(loaded_classifier.predict(np_img)[0][0])
     result = int(proba > 0.5)
     print('cnn', proba, result)
