ec82f376c8b75a29cfeb37bb405f907410cabe29
Sankalp <sink257@hotmail.com>
2023-11-03 03:31:30 +0800

    Cleanup of FE Code and add gitignore

--- SystemCode/frontend/src/components/Header.tsx
+++ SystemCode/frontend/src/components/Header.tsx
@@ -5,19 +5,11 @@ export default function Header() {
   return (
     <header className="flex justify-between items-center w-full mt-5 border-b-2 pb-7 sm:px-4 px-2">
       <Link href="/" className="flex space-x-2">
-        {/* <Image
-          alt="header text"
-          src="/imageIcon.png"
-          className="sm:w-14 sm:h-14 w-9 h-9"
-          width={36}
-          height={36}
-        /> */}
         <h1 className="sm:text-5xl text-3xl font-bold ml-2 tracking-tight">
-          AI Image Detector
+          TWO.AI
         </h1>
       </Link>
       <a
-        // href="https://vercel.com/templates/next.js/ai-photo-restorer"
         target="_blank"
         rel="noreferrer"
       >
--- SystemCode/frontend/src/pages/index.tsx
+++ SystemCode/frontend/src/pages/index.tsx
@@ -1,11 +1,9 @@
 import { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import Link from "next/link";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import SquigglyLines from "../components/SquigglyLines";
-import { Testimonials } from "../components/Testimonials";
 
 const Home: NextPage = () => {
   return (
@@ -16,15 +14,6 @@ const Home: NextPage = () => {
 
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-28 mt-20">
-        {/* <a
-          href="https://twitter.com/nutlope/status/1626074563481051136"
-          target="_blank"
-          rel="noreferrer"
-          className="border rounded-2xl py-1 px-4 text-slate-500 text-sm mb-5 hover:scale-105 transition duration-300 ease-in-out"
-        >
-          Used by over <span className="font-semibold">200,000</span> happy
-          users
-        </a> */}
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-slate-900 sm:text-7xl">
           Detecting fake images{" "}
           <span className="relative whitespace-nowrap text-[#3290EE]">
@@ -38,15 +27,6 @@ const Home: NextPage = () => {
           Not sure whether an image is authentic or AI-generated? Let our AI analyse your images to help you discern fact from fiction. Try it out now!
         </p>
         <div className="flex justify-center space-x-4">
-          <a
-            className="bg-white rounded-xl text-black font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-gray-100 border"
-            // href="https://youtu.be/FRQtFDDrUXQ"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Learn how to use
-          </a>
-
           <Link
             className="bg-black rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-black/80"
             href="/restore"
